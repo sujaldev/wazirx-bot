c43f1c99136b3f5d08b0bcc8925d57fd64786a49
Sujal Singh <email.sujalsingh@gmail.com>
2021-11-04 18:05:39 +0530

    NEW: Interactions WazirxWeb

---
+++ src/interaction_lib/interactions.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from src.interaction_lib.left_board import LeftBoard
+from src.interaction_lib.right_board import RightBoard
+
+
+class WazirxWeb:
+    URL = "https://www.wazirx.com/exchange"
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get(self.URL)
+
+        self.lb = LeftBoard(self.driver)
+        self.rb = RightBoard(self.driver)
+
+        while True:
+            command = input(">>> ").split(" ")
+            while len(command) < 3:
+                command.append("")
+            command = "self.{}.{}({})".format(*command)
+
+            try:
+                exec(command)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    WazirxWeb()
